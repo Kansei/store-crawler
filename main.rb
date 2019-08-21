1ddf8c6a87d62cdeebb36b20468d5e0bdede938a
Kansei <kk7221dev@gmail.com>
2019-08-22 08:34:50 +0900

    f

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ prefecture = '福岡'
 areas = ['大名(福岡県 福岡市中央区)', '博多駅', '天神駅', '赤坂駅（福岡県）']
 
 areas.each do |area|
+  pp 'start crawling '+area
   crawler = TabelogCrawler.new(prefecture: prefecture, area: area)
   crawler.perform
 end
